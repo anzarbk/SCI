26f0f0d1167109f4e66be0b1e55afdcddf006475
anzarbk <ansarzk4@gmail.com>
2024-02-24 23:10:26 +0530

    qwerty

--- app/(home)/Other.tsx
+++ app/(home)/Other.tsx
@@ -1,6 +1,7 @@
-import { Post } from "@prisma/client";
+
 import React from "react";
 import Card from "../(shared)/Card";
+import { Post } from "@prisma/client";
 
 type Props = {
   otherPosts: Array<Post>;
