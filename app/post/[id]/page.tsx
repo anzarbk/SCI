7a5c83abdc44e223c951984fb408ef738ee1cb1e
anzarbk <ansarzk4@gmail.com>
2024-02-24 20:23:36 +0530

    last commit

--- app/post/[id]/page.tsx
+++ app/post/[id]/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { Post as PostType } from "@prisma/client";
 import { FormattedPost } from "@/app/types";
 import Sidebar from "@/app/(shared)/Sidebar";
-// import Content from "@/app/post/[id]/Content";
+import Content from "@/app/post/[id]/Content";
 
 type Props = {
   params: { id: string };
@@ -42,7 +42,7 @@ const Post = async ({ params }: Props) => {
     <main className="px-10 leading-7">
       <div className="md:flex gap-10 mb-5">
         <div className="basis-3/4">
-          {/* <Content post={post} /> */}
+          <Content post={post} />
         </div>
         <div className="basis-1/4">
           <Sidebar />
